a29ba32924ef8058fee0394ad1ac5c70238905e7
Katasravani <105917453+Katasravani@users.noreply.github.com>
2023-11-29 15:56:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
+// script.js
+
 function insert_Row() {
-    //Write your code here
-  
-  
+    // Get the table reference by its ID
+    let table = document.getElementById('sampleTable');
+
+    // Insert a new row at the top of the table
+    let newRow = table.insertRow(0);
+
+    // Insert cells into the new row
+    let cell1 = newRow.insertCell(0);
+    let cell2 = newRow.insertCell(1);
+
+    // Set the content of the cells
+    cell1.innerHTML = 'New Cell1';
+    cell2.innerHTML = 'New Cell2';
 }
